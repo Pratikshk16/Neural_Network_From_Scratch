eedaefa5e10089c6b54b2785b407cb1f56ca9315
Your Name <you@example.com>
2025-05-09 17:31:39 +0530

    Neuron Code

---
+++ p1.py
@@ -0,0 +1,7 @@
+import sys
+import numpy as np 
+# Ensure matplotlib is installed by running 'pip install matplotlib' in the terminal.
+import matplotlib 
+print("Python: ", sys.version)
+print("Numpy: ", np.__version__)
+print("Matplotlib: ", matplotlib.__version__)
